8403d7d0c2e300d95055ca9d76eaa4b7b4004e58
Shohanul Islam <shohanulislam19892@gmail.com>
2026-05-12 04:42:00 +0600

    Admin UI: Refactor models to use Unfold ModelAdmin for modern look

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
 from .models import User, OTP, UserPreference, BusinessInformation
 
-class BusinessInformationAdmin(admin.ModelAdmin):
+class BusinessInformationAdmin(ModelAdmin):
     list_display = ['dealership_name', 'user', 'verification_status']
     list_filter = ['verification_status']
     search_fields = ['dealership_name', 'user__email']
@@ -11,16 +12,26 @@ class BusinessInformationAdmin(admin.ModelAdmin):
         queryset.update(verification_status='verified', rejection_reason=None)
         for info in queryset:
             user = info.user
-            user.is_verified = True # Assuming overall verification depends on business info for dealers
+            user.is_verified = True
             user.save()
     verify_dealers.short_description = "Mark selected dealers as Verified"
 
     def reject_dealers(self, request, queryset):
-        # In a real scenario, you'd want a form to provide the reason
         queryset.update(verification_status='rejected')
     reject_dealers.short_description = "Mark selected dealers as Rejected"
 
-admin.site.register(User)
-admin.site.register(OTP)
-admin.site.register(UserPreference)
+@admin.register(User)
+class UserAdmin(ModelAdmin):
+    list_display = ['email', 'full_name', 'is_buyer', 'is_dealer', 'is_verified']
+    search_fields = ['email', 'full_name']
+    list_filter = ['is_buyer', 'is_dealer', 'is_verified']
+
+@admin.register(OTP)
+class OTPAdmin(ModelAdmin):
+    list_display = ['user', 'code', 'created_at', 'is_used']
+
+@admin.register(UserPreference)
+class UserPreferenceAdmin(ModelAdmin):
+    list_display = ['user', 'city']
+
 admin.site.register(BusinessInformation, BusinessInformationAdmin)
