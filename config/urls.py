ebae47e8a5425ef4070b4e3003279161b2ef2c24
Shohanul Islam <shohanulislam19892@gmail.com>
2026-05-12 05:56:59 +0600

    Vehicles: Register vehicles app and API routing

--- config/urls.py
+++ config/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.py.urls if hasattr(admin, 'py') else admin.site.urls), # standard is admin.site.urls
     path('api/users/', include('users.urls')),
+    path('api/vehicles/', include('vehicles.urls')),
 ]
 
 if settings.DEBUG:
