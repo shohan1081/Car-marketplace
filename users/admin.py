bc249192a8facfdf5e6ebba9dd41b3c6c9939d3a
jafor <abujafo34@gmail.com>
2026-07-19 21:21:59 +0600

    chore(admin): customize UserAdmin to hide unused fields and show business info inline

--- users/admin.py
+++ users/admin.py
@@ -81,11 +81,27 @@ class BusinessInformationAdmin(ModelAdmin):
         queryset.update(verification_status='rejected')
     reject_dealers.short_description = "Mark selected dealers as Rejected"
 
+from unfold.admin import StackedInline
+
+class BusinessInformationInline(StackedInline):
+    model = BusinessInformation
+    can_delete = False
+    verbose_name_plural = 'Business Information'
+
 @admin.register(User)
 class UserAdmin(ModelAdmin):
     list_display = ['email', 'full_name', 'is_buyer', 'is_dealer', 'is_verified']
     search_fields = ['email', 'full_name']
     list_filter = ['is_buyer', 'is_dealer', 'is_verified']
+    inlines = [BusinessInformationInline]
+    
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('full_name', 'phone_number', 'designation', 'profile_photo', 'location')}),
+        ('Roles & Status', {'fields': ('is_buyer', 'is_dealer', 'is_verified', 'is_active')}),
+        ('Permissions', {'fields': ('is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important Dates', {'fields': ('last_login', 'date_joined')}),
+    )
 
 @admin.register(OTP)
 class OTPAdmin(ModelAdmin):
@@ -96,3 +112,4 @@ class UserPreferenceAdmin(ModelAdmin):
     list_display = ['user', 'city']
 
 admin.site.register(BusinessInformation, BusinessInformationAdmin)
+
